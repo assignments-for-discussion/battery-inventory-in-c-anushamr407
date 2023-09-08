329587f59e3dfc19a2fbe9695ad24d3dd68568f9
Anushamr407 <96069017+anushamr407@users.noreply.github.com>
2023-09-08 19:19:09 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -1,29 +1,49 @@
 #include <stdio.h>
-#include <assert.h>
 
-struct CountsBySoH {
-  int healthy;
-  int exchange;
-  int failed;
-};
-
-struct CountsBySoH countBatteriesByHealth(const int* presentCapacities, int nBatteries) {
-  struct CountsBySoH counts = {0, 0, 0};
-  return counts;
+// Function to calculate SoH (State of Health)
+double calculateSoH(double present_capacity, double rated_capacity) {
+    return (present_capacity / rated_capacity) * 100.0;
 }
 
-void testBucketingByHealth() {
-  const int presentCapacities[] = {115, 118, 80, 95, 91, 77};
-  const int numberOfBatteries = sizeof(presentCapacities) / sizeof(presentCapacities[0]);
-  printf("Counting batteries by SoH...\n");
-  struct CountsBySoH counts = countBatteriesByHealth(presentCapacities, numberOfBatteries);
-  assert(counts.healthy == 2);
-  assert(counts.exchange == 3);
-  assert(counts.failed == 1);
-  printf("Done counting :)\n");
+// Function to classify a battery based on SoH
+char* classifyBattery(double soh) {
+    if (soh > 80.0) {
+        return "Healthy";
+    } else if (soh >= 65.0) {
+        return "Replace";
+    } else {
+        return "Failed";
+    }
 }
 
 int main() {
-  testBucketingByHealth();
-  return 0;
+    double rated_capacity = 120.0; // Rated capacity of all batteries (in Ah)
+    double present_capacities[] = {105.0, 95.0, 60.0, 110.0, 75.0, 100.0};
+    int num_batteries = sizeof(present_capacities) / sizeof(present_capacities[0]);
+
+    int healthy_count = 0;
+    int replace_count = 0;
+    int failed_count = 0;
+
+    // Loop through each battery, calculate SoH, classify, and count
+    for (int i = 0; i < num_batteries; i++) {
+        double soh = calculateSoH(present_capacities[i], rated_capacity);
+        char* classification = classifyBattery(soh);
+
+        if (classification == "Healthy") {
+            healthy_count++;
+        } else if (classification == "Replace") {
+            replace_count++;
+        } else {
+            failed_count++;
+        }
+    }
+
+    printf("Classification Counts:\n");
+    printf("Healthy: %d\n", healthy_count);
+    printf("Replace: %d\n", replace_count);
+    printf("Failed: %d\n", failed_count);
+
+    return 0;
 }
+
